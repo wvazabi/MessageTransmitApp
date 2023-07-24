c1f05f98eb3c18b5e53c34f9dbe19ff060938305
Enes Kaya <eng.eneskaya@gmail.com>
2023-07-24 21:34:19 +0300

    test

--- MessageTransmitApp/MessageTransmitApp/ViewControllerDel1.swift
+++ MessageTransmitApp/MessageTransmitApp/ViewControllerDel1.swift
@@ -48,6 +48,7 @@ class ViewControllerDel1: UIViewController {
 extension ViewControllerDel1: DataTransfer{
     func printDataLabel(data: String) {
         labelDel1.text = data
+        print("test")
     }
     
     
